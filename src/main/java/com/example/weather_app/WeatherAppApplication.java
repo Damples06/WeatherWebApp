223df3c0f7e701940786638ec5f650e07f04a43c
selçuk yılmaz <selcuuukyilmaz@gmail.com>
2024-03-02 15:08:44 +0300

    feat: Implement backend
    - Added controllers for retrieving area details and daily weather data
    - Integrated services to fetch data from external APIs and store in the database
    - Utilized caching for daily weather data to improve performance
    - Implemented data models for Area and Daily entities
    - Included repository interfaces for data access
    - Updated application.properties for database configuration
    - Added necessary dependencies and configurations for Spring Boot

--- src/main/java/com/example/weather_app/WeatherAppApplication.java
+++ src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -1,13 +1,10 @@
 package com.example.weather_app;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme(value = "mytodo")
-public class WeatherAppApplication implements AppShellConfigurator {
+public class WeatherAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
